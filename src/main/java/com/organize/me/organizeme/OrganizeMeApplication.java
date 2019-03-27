5261f57457ba6d1139079de7d77eb5e8a8fed5e9
youssef smimaa <youssef.smimaa>
2019-03-27 14:30:47 +0100

    Resolve conflicts

---
+++ src/main/java/com/organize/me/organizeme/OrganizeMeApplication.java
@@ -0,0 +1,13 @@
+package com.organize.me.organizeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizeMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizeMeApplication.class, args);
+	}
+
+}
