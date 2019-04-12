cd7857c2e064065eed0c387af6cafbd4858f0d11
youssef smimaa <youssef.smimaa>
2019-04-12 17:33:15 +0200

    [service-admin] implemente handling error approche

--- src/main/java/com/organize/me/service/administration/exception/UserCustomeException.java
+++ src/main/java/com/organize/me/service/administration/exception/UserCustomeException.java
@@ -3,12 +3,20 @@ package com.organize.me.service.administration.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDate;
+
+
+@ControllerAdvice
 public class UserCustomeException extends ResponseEntityExceptionHandler {
 
-    public ResponseEntity<UserError> handleUserNotFoundException(){
-        UserError userError=new UserError(HttpStatus.NOT_FOUND.getReasonPhrase(), "","technical",String.valueOf(HttpStatus.NOT_FOUND.value()));
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<UserError> handleUserNotFoundException(UserNotFoundException ex, WebRequest request){
+        UserError userError=new UserError(ex.getMessage(), LocalDate.now(),"404",request.getDescription(false));
         return new ResponseEntity<>(userError,HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/organize/me/service/administration/resource/UserRestController.java
+++ src/main/java/com/organize/me/service/administration/resource/UserRestController.java
@@ -1,55 +1,68 @@
 package com.organize.me.service.administration.resource;
 
 
+import com.organize.me.service.administration.exception.UserNotFoundException;
 import com.organize.me.service.administration.model.User;
 import com.organize.me.service.administration.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/users")
-public class UserRestController {
+public class UserRestController implements IUserRestController {
 
     @Autowired
     private UserService userService;
 
+    @Override
     @GetMapping(value = "/byUsername/{username}")
-    public ResponseEntity<User> findUserByUsername(@PathVariable("username") String username) {
+    public ResponseEntity<User> findUserByUsername(String username) {
         Optional<User> userByUsername = Optional.empty();
         if (!StringUtils.isEmpty(username)) {
             userByUsername = userService.findUserByUsername(username);
+            if (!userByUsername.isPresent()) {
+                throw new UserNotFoundException("User " + username + " not find");
+            }
         }
         return new ResponseEntity<>(userByUsername.get(), HttpStatus.OK);
     }
 
     @GetMapping
+    @Override
     public ResponseEntity<Stream<User>> findAllUsers() {
         Stream<User> users = userService.fetchUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @GetMapping(value = "/byId/{id}")
-    public ResponseEntity<Optional<User>> findUserById(@PathVariable("id") Long id) {
+    @Override
+    public ResponseEntity<Optional<User>> findUserById(Long id) {
         Optional<User> userToFind = Optional.empty();
         if (id != null) {
             userToFind = userService.findById(id);
+            if (!userToFind.isPresent()) {
+                throw new UserNotFoundException("User " + id + " not find");
+            }
         }
         return new ResponseEntity<>(userToFind, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Optional<User>> createUser(@RequestBody User user) {
+    @Override
+    public ResponseEntity<Optional<User>> createUser(User user) {
         Optional<User> userToCreate = Optional.empty();
         if (user != null) {
             userToCreate = userService.create(user);
         }
         return new ResponseEntity<>(userToCreate, HttpStatus.OK);
     }
-
 }
