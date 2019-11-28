b1d31a64fcc30679d8f90eb500ec1466f3126c6c
youssef smimaa <youssef.smimaa>
2019-11-28 16:16:13 +0100

    [service-admin] update error handling

--- src/main/java/com/service/admin/exception/UserCustomizedException.java
+++ src/main/java/com/service/admin/exception/UserCustomizedException.java
@@ -1,18 +1,14 @@
 package com.service.admin.exception;
 
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDate;
-
 
 @ControllerAdvice
 public class UserCustomizedException extends ResponseEntityExceptionHandler {
@@ -21,12 +17,5 @@ public class UserCustomizedException extends ResponseEntityExceptionHandler {
     public ResponseEntity<UserError> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
         UserError userError = new UserError(ex.getMessage(), LocalDate.now(), "404", request.getDescription(false));
         return new ResponseEntity<>(userError, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        BindingResult bindingResult = ex.getBindingResult();
-        bindingResult.getFieldError();
-        return super.handleMethodArgumentNotValid(ex, headers, status, request);
     }
 }
